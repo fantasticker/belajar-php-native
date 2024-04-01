fa49878034c02ac5d614a1dab0744da5243f9f3c
fantasticker <varrelvs04@gmail.com>
2024-04-01 23:51:53 +0700

    database update

---
+++ php_dasar.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Apr 2024 pada 18.46
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_dasar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nrp` char(10) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `jurusan` varchar(64) NOT NULL,
+  `gambar` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nrp`, `nama`, `email`, `jurusan`, `gambar`) VALUES
+(1, '2010512033', 'Vissi', '2010512033@mahasiswa.upnvj.ac.id', 'S-1 Ilmu Pedang Teyvat', '65fa8a0269026.jpg'),
+(2, '2010512034', 'Vissi x Sakhira', '2010512034@upnvj.ac.id', 'S-1 Matematika', 'gambar2.JPG'),
+(3, '2010512035', 'Vissi x Cornelia', '2010512035@upnvj.ac.id', 'S-1 Hubungan Internasional', 'gambar3.JPG'),
+(4, '2010512036', 'Vissi x Catel', '2010512036@upnvj.ac.id', 'S-1 Ilmu Komunikasi', 'gambar4.JPG'),
+(5, '2010512037', 'Vissi x Icha', '2010512037@mahasiswa.upnvj.ac.id', 'S-1 Manajemen', 'gambar5.JPG'),
+(20, '123', '123', '123', '123', 'gambar6.JPG');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'vissi', '$2y$10$0Gz9ciNuq7d/igJXjomjPONt4csEG99tk6f1nymo6dvc3aLQhuzPe'),
+(2, 'admin', '$2y$10$nOuXRdbTziIFtW63FWIgGuVSDKDeaWdLiQG438AkDE8GYm2TDPLKS');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
